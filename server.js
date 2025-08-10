1510750dda06cab4b52e94beaa790bce72325a24
DropNGo <zukurz223@gmail.com>
2025-08-11 01:16:13 +0200

    Konflikte in package-lock.json und server.js gelöst

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -17,27 +16,5 @@ app.get("/", (req, res) => {
 
 // Server starten
 app.listen(PORT, () => {
-  console.log('✅ Server läuft auf http://localhost:' + PORT);
+  console.log(`✅ Server läuft auf http://localhost:${PORT}`);
 });
-=======
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-
-const app = express();
-const PORT = 3000;
-
-// Middleware
-app.use(cors());
-app.use(express.static(path.join(__dirname, "frontend")));
-
-// Startseite
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
-});
-
-// Server starten
-app.listen(PORT, () => {
-  console.log('✅ Server läuft auf http://localhost:' + PORT);
-});
->>>>>>> ef080fb (Erster Commit mit meinem DropNGo Projekt)
