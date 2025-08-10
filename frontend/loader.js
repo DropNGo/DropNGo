83c8f9cd39cd10ce93864fca8779a0f922894978
DropNGo <zukurz223@gmail.com>
2025-08-11 00:12:59 +0200

    Konflikte gelöst

--- frontend/loader.js
+++ frontend/loader.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.addEventListener('load', () => {
   const loader = document.getElementById('page-loader');
   const mainContent = document.getElementById('main-content');
@@ -22,3 +23,29 @@ window.addEventListener('load', () => {
 
 
 
+=======
+window.addEventListener('load', () => {
+  const loader = document.getElementById('page-loader');
+  const mainContent = document.getElementById('main-content');
+
+  // Nach 2.2 Sekunden (etwas länger als Animation) Loader ausblenden
+  setTimeout(() => {
+    loader.classList.add('hide');
+    mainContent.classList.add('visible');
+  }, 2200);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> ef080fb (Erster Commit mit meinem DropNGo Projekt)
